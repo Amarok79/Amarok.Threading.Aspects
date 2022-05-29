2b3ad7b879f08205356bb9de8b762807acefd78e
Olaf Kober <olaf.kober@outlook.com>
2022-05-29 12:08:40 +0200

    prototyping

---
+++ src/Amarok.Threading.Aspects/Threading.Aspects/SlimThreadAffine.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2022, Olaf Kober <olaf.kober@outlook.com>
+
+#pragma warning disable IDE1006 // Naming Styles
+#pragma warning disable 0169
+
+using System;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using Metalama.Framework.Eligibility;
+
+
+namespace Amarok.Threading.Aspects;
+
+
+[Inherited]
+public class SlimThreadAffine : TypeAspect
+{
+    public override void BuildEligibility(IEligibilityBuilder<INamedType> builder)
+    {
+        base.BuildEligibility(builder);
+
+        builder.MustBeNonStatic();
+    }
+
+    public override void BuildAspect(IAspectBuilder<INamedType> builder)
+    {
+        base.BuildAspect(builder);
+
+        foreach (var property in builder.Target.Properties)
+        {
+            if (property.IsStatic)
+            {
+                continue;
+            }
+
+            if (property.Writeability != Writeability.All)
+            {
+                continue;
+            }
+
+
+            builder.Advice.OverrideAccessors(property, nameof(FieldGetTemplate), nameof(FieldSetTemplate));
+        }
+    }
+
+
+    [Template]
+    public dynamic? FieldGetTemplate()
+    {
+        __SlimThreadAffine_VerifyAccess(meta.Target.Member.ToDisplayString());
+
+        return meta.Proceed();
+    }
+
+    [Template]
+    public void FieldSetTemplate(dynamic value)
+    {
+        __SlimThreadAffine_VerifyAccess(meta.Target.Member.ToDisplayString());
+
+        meta.Proceed();
+    }
+
+
+
+    [Introduce(WhenExists = OverrideStrategy.Ignore)]
+    private protected readonly Int32 __SlimThreadAffine_ManagedThreadId = Environment.CurrentManagedThreadId;
+
+
+    [Introduce(WhenExists = OverrideStrategy.Ignore)]
+    private protected void __SlimThreadAffine_VerifyAccess(String memberName)
+    {
+        if (Environment.CurrentManagedThreadId == __SlimThreadAffine_ManagedThreadId)
+        {
+            return;
+        }
+
+        __SlimThreadAffine_Throw(memberName);
+    }
+
+    [Introduce(WhenExists = OverrideStrategy.Ignore)]
+    private protected void __SlimThreadAffine_Throw(String memberName)
+    {
+        throw new InvalidOperationException(
+            $"Thread '{Environment.CurrentManagedThreadId}' is not allowed to access member '{memberName}' " +
+            $"on object '{ToString()}' of type '{GetType().AssemblyQualifiedName}' as the object is bound " +
+            $"to thread '{__SlimThreadAffine_ManagedThreadId}'."
+        );
+    }
+}
---
+++ src/Amarok.Threading.Aspects/Threading.Aspects/SlimThreadAffineSample.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) 2022, Olaf Kober <olaf.kober@outlook.com>
+
+#pragma warning disable LAMA0048
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Amarok.Threading.Aspects;
+
+
+[SlimThreadAffine]
+public class SlimThreadAffineSample
+{
+    // Fields
+
+    public const Int32 ConstField = -1;
+
+    public static readonly Int32 StaticReadOnlyField = -1;
+
+    public static Int32 StaticField = -1;
+
+    public readonly Int32 MemberReadOnlyField = 123;
+
+    public Int32 MemberField;
+
+
+    // Properties
+
+    public static Int32 StaticGetSetProperty { get; set; }
+
+    public static Int32 StaticGetProperty { get; }
+
+    public static Int32 StaticSetProperty
+    {
+        set => _ = value;
+    }
+
+    public Int32 MemberGetSetProperty { get; set; }
+
+    public Int32 MemberGetInitProperty { get; init; }
+
+    public Int32 MemberGetPrivateSetProperty { get; private set; }
+
+    public Int32 MemberGetProperty { get; }
+
+    public Int32 MemberSetProperty
+    {
+        set => _ = value;
+    }
+
+    public Int32 this[Int32 index]
+    {
+        get => -1;
+        set => _ = value;
+    }
+
+    // Methods
+
+    public static void StaticMethod()
+    {
+    }
+
+    public void MemberMethod()
+    {
+    }
+
+    public Int32 MemberReturnMethod()
+    {
+        return -1;
+    }
+
+    public IEnumerable<Int32> MemberIterator()
+    {
+        yield return 123;
+        yield return 456;
+    }
+
+    public async Task MemberAsyncMethod()
+    {
+        await Task.Yield();
+        await Task.Yield();
+    }
+
+    public async Task<Int32> MemberAsyncReturnMethod()
+    {
+        await Task.Yield();
+        await Task.Yield();
+
+        return -1;
+    }
+
+    public async IAsyncEnumerable<Int32> MemberAsyncIterator()
+    {
+        await Task.Yield();
+
+        yield return 123;
+
+        await Task.Yield();
+
+        yield return 456;
+    }
+}
+
+//[SlimThreadAffine]
+public class SlimThreadAffineSampleSub : SlimThreadAffineSample
+{
+    public Int32 SubMemberGetSetProperty { get; set; }
+}
